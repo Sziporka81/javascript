cca24debcf45813d5b362990929005dd42409fa6
Sziporka81 <totheszter@turr.hu>
2021-03-25 20:54:46 +0100

    2.feladat

---
+++ JSesemenyek.js
@@ -0,0 +1,14 @@
+/*1.feladat: Egy gombra kattintva kérd be a felhasználó nevét
+ egy felugró ablakban, majd írd ki egy p tag-be, üdvözöllek + neve. */
+
+ function myFunction(){
+     let nev=prompt("Kérem adja meg a nevét:");
+     document.getElementById("first").innerHTML=("Üdvözlöm " + nev);
+ }
+
+ /*2.fealadat: Illessz be egy tetszőleges képet, 
+ és hozz létre egy eseményt.*/
+
+ function pictureFunction(){
+     document.getElementById("picture");
+ }
